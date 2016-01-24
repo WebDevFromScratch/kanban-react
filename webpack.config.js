379b9a96b00f55ec99f5349ac8c4dc82b0d5e996
Piotr Klosinski <mybettingstuff@gmail.com>
2016-01-24 21:18:48 +0100

    set HMR up, remove now unnecessary part of config from package.json (moved to webpack config)

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,29 @@ const common = {
 };
 
 if (TARGET === 'start' || !TARGET) {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env so this is easy to customize.
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
 }
 
 if (TARGET === 'build') {
