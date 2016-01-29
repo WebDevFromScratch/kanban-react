0666313717ce5d17a03b4c8b7c525bb64113c081
Piotr Klosinski <mybettingstuff@gmail.com>
2016-01-29 22:58:20 +0100

    add webpack config for babel

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ const PATHS = {
 const common = {
   // Entry accepts a path or an object of entries.
   entry: PATHS.app,
+
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -35,6 +42,14 @@ const common = {
         test: /\.css$/,
         loaders: ['style', 'css'],
         // Include accepts either a path or an array of paths.
+        include: PATHS.app
+      },
+      {
+        test: /\.jsx?$/,
+        // Enable caching for improved performance during development
+        // It uses default OS directory by default. If you need something
+        // more custom, pass a path to it. I.e., babel?cacheDirectory=<path>
+        loaders: ['babel?cacheDirectory'],
         include: PATHS.app
       }
     ]
