ded333e3a065fdcc2c99ebd8b94e9316fe335cf9
Piotr Klosinski <mybettingstuff@gmail.com>
2016-01-30 19:32:04 +0100

    add dummy data and render it for the time being

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,32 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx'
+// import Note from './Note.jsx';
+
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn Webpack'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Learn React'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Do laundry'
+  }
+]
 
 export default class App extends React.Component {
   render () {
-    return <Note />;
+    // return <Note />;
+
+    return (
+      <div>
+        <ul>{notes.map(note =>
+          <li key={note.id}>{note.task}</li>
+        )}</ul>
+      </div>
+    )
   }
 }
