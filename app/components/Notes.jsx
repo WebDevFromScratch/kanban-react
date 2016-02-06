49c95536eff72ed75a71cb8404b8048570d1045e
Piotr Klosinski <mybettingstuff@gmail.com>
2016-02-06 18:46:58 +0100

    refactor Notes component for the redux flow

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux'
 
-import Note from './Note.jsx';
+// import Note from './Note.jsx';
 
-export default ({notes, onEdit, onDelete}) => {
-  return(
-    <ul className="notes">
-      {notes.map(note =>
-        <li className="note" key={note.id}>
-          <Note
-            task={note.task}
-            onEdit={onEdit.bind(null, note.id)}
-            onDelete={onDelete.bind(null, note.id)} />
-        </li>
-      )}
-    </ul>
-  );
+class Notes extends React.Component {
+  render() {
+    const notes = this.props.notes;
+
+    return(
+      <ul className="notes">
+        {notes.map(note =>
+          <li className="note" key={note.id}>
+            {note.task}
+            {/*<Note task={note.task} />*/}
+          </li>
+        )}
+      </ul>
+    );
+  }
 }
+Notes = connect()(Notes);
+
+export default Notes;
