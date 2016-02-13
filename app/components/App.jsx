6eedf469c336fd0e38672cf70e7709d9ae6b67db
Piotr Klosinski <mybettingstuff@gmail.com>
2016-02-13 08:15:00 +0100

    edit App component, to start using Lanes component at the root (WIP)

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,32 +1,44 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import AddNote from './AddNote.jsx';
-import Notes from './Notes.jsx';
-import * as actions from '../actions/notes';
+// import AddNote from './AddNote.jsx';
+// import Notes from './Notes.jsx';
+// import * as actions from '../actions/notes';
+
+import Lanes from './Lanes.jsx';
+import * as laneActions from '../actions/lanes';
 
 class App extends React.Component {
   render() {
     return(
       <div>
-        <AddNote onButtonClick={this.props.createNote} />
-        <Notes
-          notes={this.props.notes}
-          onValueClick={id => this.props.updateNote({id, editing: true})}
-          onEdit={(id, task) => this.props.updateNote({id, task, editing: false})}
-          onDelete={id => this.props.deleteNote({id})}/> {/*I can't quite grasp how this works exactly...*/}
+        <button className="add-lane" onClick={this.props.createLane}>+</button>
+        <Lanes />
       </div>
-    );
+    )
+
+    // return(
+    //   <div>
+    //     <AddNote onButtonClick={this.props.createNote} />
+    //     <Notes
+    //       notes={this.props.notes}
+    //       onValueClick={id => this.props.updateNote({id, editing: true})}
+    //       onEdit={(id, task) => this.props.updateNote({id, task, editing: false})}
+    //       onDelete={id => this.props.deleteNote({id})}/> {/*I can't quite grasp how this works exactly...*/}
+    //   </div>
+    // );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
-    notes: state
+    notes: state.notes,
+    lanes: state.lanes
   }
 }
 
-const mapDispatchToProps = actions;
+const mapDispatchToProps = laneActions;
+// const mapDispatchToProps = actions;
 
 App = connect(mapStateToProps, mapDispatchToProps)(App);
 
