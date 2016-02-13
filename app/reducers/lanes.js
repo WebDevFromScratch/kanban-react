5a7dbb0d563f58c1a4f7ac5084821b7053273a78
Piotr Klosinski <mybettingstuff@gmail.com>
2016-02-13 15:46:51 +0100

    add necessary logic to lanes reducer

--- app/reducers/lanes.js
+++ app/reducers/lanes.js
@@ -1,15 +1,48 @@
-import * as laneTypes from '../actions/lanes';
+import * as types from '../actions/lanes';
 
 const lanes = (state = [], action) => {
   switch(action.type) {
-    case laneTypes.CREATE_LANE:
+    case types.CREATE_LANE:
       return [
         ...state,
         {
           id: action.id,
-          name: action.name
+          name: action.name,
+          notes: []
         }
       ];
+    case types.ATTACH_TO_LANE:
+      const laneId = action.laneId;
+      const noteId = action.noteId;
+
+      return state.map((lane) => {
+        const index = lane.notes.indexOf(noteId);
+
+        if(index >= 0) {
+          return Object.assign({}, lane, {
+            notes: lane.notes.length > 1 ? lane.notes.slice(0, index).concat(
+              lane.notes.slice(index + 1)
+            ): []
+          });
+        }
+        if(lane.id === laneId) {
+          return Object.assign({}, lane, {
+            notes: [...lane.notes, noteId]
+          });
+        }
+
+        return lane;
+      });
+    case types.DETACH_FROM_LANE:
+      return state.map((lane) => {
+        if(lane.id === action.laneId) {
+          return Object.assign({}, lane, {
+            notes: lane.notes.filter((id) => id !== action.noteId)
+          });
+        }
+
+        return lane;
+      });
     default:
       return state;
   }
