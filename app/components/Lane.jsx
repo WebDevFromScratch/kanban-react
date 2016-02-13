ab04d8310d5eec5a703157636068747ddc5b152f
Piotr Klosinski <mybettingstuff@gmail.com>
2016-02-13 15:49:08 +0100

    extend Lane component, with proper adding/removing notes to a particular lane logic

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,28 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import AddNote from './AddNote.jsx';
 import Notes from './Notes.jsx';
 import * as noteActions from '../actions/notes';
+import * as laneActions from '../actions/lanes';
 
 class Lane extends React.Component {
   render() {
-    const {lane, ...props} = this.props;
+    const {lane, allNotes, ...props} = this.props;
+    const laneId = lane.id;
+    const laneNotes = lane.notes.map((id) => allNotes[
+      allNotes.findIndex((note) => note.id === id)
+    ]).filter((note) => note);
 
     return(
       <div className="lane">
         {this.props.lane.name}
-        <AddNote onButtonClick={this.props.createNote} />
+        <AddNote onButtonClick={this.addNote.bind(this, laneId)} />
         <Notes
-          notes={this.props.notes}
+          notes={laneNotes}
           onValueClick={id => this.props.updateNote({id, editing: true})}
           onEdit={(id, task) => this.props.updateNote({id, task, editing: false})}
-          onDelete={id => this.props.deleteNote({id})} />
+          onDelete={id => this.removeNote(laneId, id)} />
       </div>
     )
   }
+
+  addNote(laneId, e) {
+    e.stopPropagation();
+
+    const note = this.props.createNote();
+    this.props.attachToLane(laneId, note.id);
+  }
+
+  removeNote(laneId, noteId) {
+    this.props.detachFromLane(laneId, noteId);
+    this.props.deleteNote(noteId);
+  }
 }
 
-Lane = connect(null, noteActions)(Lane);
+const mapStateToProps = (state) => {
+  return {
+    allNotes: state.notes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(Object.assign({}, laneActions, noteActions), dispatch);
+}
+
+Lane = connect(mapStateToProps, mapDispatchToProps)(Lane);
 
 export default Lane;
