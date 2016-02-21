0f9fad25ea774a1b4ccf8ea7e02b797dced14d0d
Piotr Klosinski <mybettingstuff@gmail.com>
2016-02-21 20:40:10 +0100

    remove unnecessary redux pieces from Note

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
 class Note extends React.Component {
   render() {
     return <li {...this.props}>{this.props.children}</li>;
   }
 }
-Note = connect()(Note);
 
 export default Note;
